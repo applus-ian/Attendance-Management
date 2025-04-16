02b589a932ca77285ec8dedd46d5e4a5ffbd993f
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-16 23:05:13 +0800

    chore(sanctum)!: restrict sanctum authentication to personal access tokens only
    BREAKING CHANGE: Session-based authentication has been disabled. Only Personal Access Tokens are now supported.

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -15,11 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
+    'stateful' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -33,7 +29,7 @@ return [
     |
     */
 
-    'guard' => ['web'],
+    'guard' => ['api'],
 
     /*
     |--------------------------------------------------------------------------
@@ -77,7 +73,7 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'validate_csrf_token' => null
     ],
 
 ];
