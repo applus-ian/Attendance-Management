9e9d7081ad640bce2937c8ccef74be7c933b3eed
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:46:44 +0800

    feat(schedules): update model with relationships and attributes

--- backend/app/Models/Schedules.php
+++ backend/app/Models/Schedules.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedules extends Model
 {
-    //
+    protected $primaryKey = 'sched_id';
+
+    protected $fillable = [
+        'sched_start',
+        'sched_end',
+        'sched_break',
+        'day',
+        'num_assigned',
+    ];
+
+    public function assignedEmployees()
+    {
+        return $this->hasMany(AssignedSchedules::class, 'sched_id');
+    }
 }
