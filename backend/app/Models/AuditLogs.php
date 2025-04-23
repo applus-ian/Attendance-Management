b9dbf4ef09bde0973f8d1cf673db1884a2dac9ec
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:58:42 +0800

    feat(audit logs): update model with relationships and attributes

--- backend/app/Models/AuditLogs.php
+++ backend/app/Models/AuditLogs.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class AuditLogs extends Model
 {
-    //
+    protected $primaryKey = 'log_id';
+
+    protected $fillable = [
+        'user_id',
+        'role',
+        'action_type',
+        'target_type',
+        'target_id',
+        'description',
+        'ip_address',
+        'device',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
