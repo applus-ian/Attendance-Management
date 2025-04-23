1d03c5bc886e7d83931915b49aa93842379ee0cc
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:52:51 +0800

    feat(manual time requests): update model with relationships and attributes

--- backend/app/Models/ManualTimeRequests.php
+++ backend/app/Models/ManualTimeRequests.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class ManualTimeRequests extends Model
 {
-    //
+    protected $primaryKey = 'request_id';
+
+    protected $fillable = [
+        'emp_id',
+        'request_type',
+        'request_time',
+        'request_end',
+        'reason',
+        'approval_status',
+        'reviewed_by',
+        'reviewed_at',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'emp_id');
+    }
+
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewed_by');
+    }
 }
