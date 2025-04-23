877cb49b71d2e1ad05f5c01095f30d84d36107fd
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:45:41 +0800

    feat(departments): update model with relationships and attributes

--- backend/app/Models/Departments.php
+++ backend/app/Models/Departments.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Departments extends Model
 {
-    //
+    protected $primaryKey = 'dept_id';
+
+    protected $fillable = ['dept_name', 'manager_id'];
+
+    public function manager()
+    {
+        return $this->belongsTo(Employee::class, 'manager_id');
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class, 'dept_id');
+    }
 }
