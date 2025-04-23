cf5974c579e9fc2ad08091b072c04852c2d5aa83
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:50:04 +0800

    feat(timesheets): update model with relationships and attributes

--- backend/app/Models/Timesheets.php
+++ backend/app/Models/Timesheets.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Timesheets extends Model
 {
-    //
+    protected $primaryKey = 'timesheet_id';
+
+    protected $fillable = [
+        'emp_id',
+        'date',
+        'total_work_hrs',
+        'break_duration',
+        'overtime_hrs',
+        'total_lates',
+        'approved_by',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'emp_id');
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
 }
