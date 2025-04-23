2726148c07eafd90ef60262003949d3c10df4895
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:06:24 +0800

    feat: modify migrations for users table

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('user_id');
+            $table->unsignedBigInteger('emp_id')->unique();
+            $table->foreign('emp_id')->references('emp_id')->on('employees')->onDelete('cascade');
+            $table->string('email', 100)->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->enum('role', ['employee', 'admin', 'super_admin'])->default('employee');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps(0);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
