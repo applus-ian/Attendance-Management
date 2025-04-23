557640d112bc9ec2dd56249a371c50fb33b5815c
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 11:00:29 +0800

    feat(notifications): update model with relationships and attributes

--- backend/app/Models/Notifications.php
+++ backend/app/Models/Notifications.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notifications extends Model
 {
-    //
+    protected $primaryKey = 'notif_id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'emp_id',
+        'notif_type',
+        'notif_status',
+        'created_at',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'emp_id');
+    }
 }
