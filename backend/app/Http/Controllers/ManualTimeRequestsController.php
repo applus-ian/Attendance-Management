1d1ce76024012ba535ff3a1f20c1c89079912182
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:22:45 +0800

    feat: add migration, controller and model, and modify the migration for manual time requests table

---
+++ backend/app/Http/Controllers/ManualTimeRequestsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ManualTimeRequests;
+use Illuminate\Http\Request;
+
+class ManualTimeRequestsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ManualTimeRequests $manualTimeRequests)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ManualTimeRequests $manualTimeRequests)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ManualTimeRequests $manualTimeRequests)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ManualTimeRequests $manualTimeRequests)
+    {
+        //
+    }
+}
