bea017dda666a8a21e6451214ac7bf5f83a09b22
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-23 10:48:52 +0800

    feat(timelogs): update model with relationships and attributes

--- backend/app/Models/Timelogs.php
+++ backend/app/Models/Timelogs.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Timelogs extends Model
 {
-    //
+    protected $primaryKey = 'log_id';
+
+    public $timestamps = false;
+
+    protected $fillable = ['emp_id', 'log_type', 'log_time', 'created_by', 'is_absent'];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'emp_id');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
