389fe9b67e65876460f36302c8e75eb8d28ad96a
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-25 11:54:06 +0800

    chore(EM-backend): add backend from employee management team

---
+++ EM-backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::controller(AuthController::class)
+    ->prefix('auth')
+    ->group(function () {
+        Route::post('/validate-token', 'validateToken');
+        Route::post('/login', 'login');
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', 'logout');
+            Route::get('/fetch-user', 'fetchUser');
+        });
+});
