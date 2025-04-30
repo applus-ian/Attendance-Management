5fa3d352253620032f8a3951efaf2d18fe3a610a
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-30 09:48:44 +0800

    chore(employee_address): modify models

--- AM-backend/app/Models/EmployeeAddress.php
+++ AM-backend/app/Models/EmployeeAddress.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmployeeAddress extends Model
 {
-    //
+    protected $primaryKey = 'address_id';
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class, 'address_id');
+    }
 }
