12da5b81d5385cf6bd3a904438b25351fece547d
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-30 09:48:21 +0800

    chore(audit_logs): modify models

--- AM-backend/app/Models/AuditLogs.php
+++ AM-backend/app/Models/AuditLogs.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AuditLogs extends Model
 {
@@ -22,5 +23,10 @@ class AuditLogs extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class, 'role_id');
     }
 }
