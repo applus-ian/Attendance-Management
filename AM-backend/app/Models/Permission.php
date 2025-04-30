0e48d4ae2211cfa8e0f2d51415a308fade795cb6
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-30 09:49:27 +0800

    chore(permission): modify models

--- AM-backend/app/Models/Permission.php
+++ AM-backend/app/Models/Permission.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
-    //
+    protected $primaryKey = 'permission_id';
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'role_permission', 'permission_id', 'role_id');
+    }
 }
