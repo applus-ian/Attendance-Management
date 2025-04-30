4204dd9e5a3312c62d40397e49fdb90453770340
ValerianoS26 <valeyaustinesenoy@gmail.com>
2025-04-30 09:49:49 +0800

    chore(role): modify models

--- AM-backend/app/Models/Role.php
+++ AM-backend/app/Models/Role.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
-    //
+    protected $primaryKey = 'role_id';
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class, 'role_permission', 'role_id', 'permission_id');
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'user_role', 'role_id', 'user_id');
+    }
+
+    public function auditLogs(): HasMany
+    {
+        return $this->hasMany(AuditLog::class, 'role_id');
+    }
 }
